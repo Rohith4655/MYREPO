e5e6f8bfab1c974fa95f1897dd984afb30089b71
rohith burra <rohithburra321@gmail.com>
2021-09-17 18:59:22 +0530

    first commit

--- ModelBackend/firstPage/views.py
+++ ModelBackend/firstPage/views.py
@@ -11,13 +11,12 @@ model=joblib.load('modelPipeline.pkl')
 
 
 def scoreJson(request):
-    print (request.body)
     data =json.loads(request.body)
-    dataF=pd.DataFrame({'x':data}).transpose()
+    dataF=pd.DataFrame(data,index=[0])
     score=model.predict_proba(dataF)[:,-1][0]
     score=float(score)
-
-    return JsonResponse({'score':score})
+    print(score)
+    return JsonResponse({'score':1})
 
 
 def scoreFile(request):
@@ -26,12 +25,12 @@ def scoreFile(request):
     filePathName=fs.save(fileObj.name,fileObj)
     filePathName=fs.url(filePathName)
     filePath='.'+filePathName
-
     data =pd.read_csv(filePath)
-    score=model.predict_proba(data)[:,-1]
+    data2=data.columns[1:]
+    score=model.predict_proba(data[data2])[:,-1]
+    print(data[data2])
+    # score={j:k for j,k in zip(data['Loan_ID'],score)}
 
-    score={j:k for j,k in zip(data['Loan_ID'],score)}
-
-    score =sorted(score.items(),key=lambda x: x[1],reverse=True)
-
-    return JsonResponse({'result':score})
+    # score =sorted(score.items(),key=lambda x: x[1],reverse=True)
+    score.sort()
+    return JsonResponse({'result':1})
